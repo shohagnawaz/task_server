7d12566d9f6ff22bc204659a3fc9d73a1f7f7c10
shohagnawaz <snawazdigital@gmail.com>
2026-01-17 19:46:00 +0600

    vehicles

--- src/server.ts
+++ src/server.ts
@@ -58,13 +58,117 @@ app.get('/', (req: Request, res: Response) => {
     res.send('Hello Next Level Developers!')
 });
 
-app.post("/", (req: Request, res: Response) => {
-    console.log(req.body);
+// Users CRUD
+app.post("/api/v1/users", async (req: Request, res: Response) => {
+    const { name, email, password, phone, role } = req.body;
 
-    res.status(201).json({
+    try {
+        const result = await pool.query(`INSERT INTO users(name, email, password, phone, role) VALUES($1, $2, $3, $4, $5) RETURNING *`, [name, email, password, phone, role]);
+
+        res.status(201).json({
         success: true,
-        message: "API is working"
-    })
+        message: "User inserted successfully",
+        data: result.rows[0]
+        });
+
+    } catch (err: any) {
+        res.status(500).json({
+            success: false,
+            message: err.message
+        })
+    }
+});
+// Users CRUD
+app.get("/api/v1/users", async (req: Request, res: Response) => {
+    try {
+        const result = await pool.query(`SELECT * FROM users`);
+        res.status(200).json({
+            success: true,
+            message: "Users fetched successfully",
+            data: result.rows
+        })
+    } catch(err: any) {
+        res.status(500).json({
+            success: false,
+            message: err.message
+        })     
+    }
+});
+// Users CRUD
+app.put("/api/v1/users/:id", async (req: Request, res: Response) => {
+    const { name, email, password, phone, role } = req.body;
+
+    try {
+        const result = await pool.query(`UPDATE users SET name=$1, email=$2, password=$3, phone=$4, role=$5 WHERE id=$6 RETURNING *`, [
+            name, email, password, phone, role, req.params.id
+        ]);
+
+        if(result.rows.length === 0) {
+            res.status(404).json({
+                success: false,
+                message: "User not found"
+            })
+        } else {
+            res.status(200).json({
+                success: true,
+                message: "User update successfully",
+                data: result.rows[0]
+            })
+        }
+
+    } catch(err: any) {
+        res.status(500).json({
+            success: false,
+            message: err.message
+        })
+    }
+});
+// Users CRUD
+app.delete("/api/v1/users/:id", async (req: Request, res: Response) => {
+    try {
+        const result = await pool.query(`DELETE FROM users WHERE id = $1`, [
+            req.params.id
+        ]);
+        
+        if(result.rowCount === 0) {
+            res.status(404).json({
+                success: false,
+                message: "User not found"
+            });
+        } else {
+            res.status(200).json({
+                success: true,
+                message: "User deleted successfully",
+                data: null
+            });
+        }
+
+    } catch(err: any) {
+        res.status(500).json({
+            success: false,
+            message: err.message
+        })
+    }
+});
+
+// Vehicles CRUD
+app.post("/api/v1/vehicles", async (req: Request, res: Response) => {
+    const { vehicle_name, type, registration_number, daily_rent_price, availability_status } = req.body;
+    try {
+        const result = await pool.query(`INSERT INTO vehicles(vehicle_name, type, registration_number, daily_rent_price, availability_status) VALUES($1, $2, $3, $4, $5) RETURNING *`, [vehicle_name, type, registration_number, daily_rent_price, availability_status]);
+
+        res.status(201).json({
+            success: true,
+            message: "Vehicle inserted successfully",
+            data: result.rows[0]
+        })
+
+    } catch(err: any) {
+        res.status(500).json({
+            success: false,
+            message: err.message
+        })
+    }
 });
 
 app.listen(port, () => {
